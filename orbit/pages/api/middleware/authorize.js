13af1096e96269829f6322fd216728316f07ff6a
German Solano <kontrolg1239@gmail.com>
2022-05-19 07:36:40 -0400

    feat(Server): :sparkles: Add middleware to check if the user is authenticated or not

--- orbit/pages/api/middleware/authorize.js
+++ orbit/pages/api/middleware/authorize.js
@@ -1,11 +1,21 @@
-import nextConnect from 'next-connect';
+import { parse } from "cookie";
+import { verify } from "jsonwebtoken";
+import nextConnect from "next-connect";
 
 async function authorize(req, res, next) {
   try {
+    const cookies = parse(req?.headers?.cookie) || {};
+    const token = cookies?.token;
+
+    if (!token) {
+      throw new Error("Unauthorized");
+    }
+
+    const decoded = verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    return next();
   } catch (err) {
-    return res
-      .status(401)
-      .json({ message: 'Not authorized' });
+    return res.status(401).json({ message: "Not authorized" });
   }
 }
 
