26fe1de4bed815d40e82f765080bea0fa7f7e008
German Solano <kontrolg1239@gmail.com>
2022-05-19 07:53:18 -0400

    feat(Server): :sparkles: Add middleware to check if a user has an admin role

--- orbit/pages/api/middleware/authorize-admin.js
+++ orbit/pages/api/middleware/authorize-admin.js
@@ -1,15 +1,16 @@
-import nextConnect from 'next-connect';
-
-async function authorizeAdmin(req, res, next) {
-  try {
-  } catch (err) {
-    return res
-      .status(401)
-      .json({ message: 'Not authorized' });
-  }
-}
+import nextConnect from "next-connect";
+import authorize from "./authorize";
 
 const middleware = nextConnect();
+
+middleware.use(authorize);
+
+async function authorizeAdmin(req, res, next) {
+  if (!req?.user?.role || req?.user?.role !== "admin") {
+    return res.status(401).json({ message: "Insufficient Role" });
+  }
+  return next();
+}
 
 middleware.use(authorizeAdmin);
 
