1a9e74bf1c4c0c9e0687351bed819e999f686a63
German Solano <kontrolg1239@gmail.com>
2022-05-21 06:47:41 -0400

    feat(Serverless): :sparkles: Add util to check if user is authenticated and add it to the get dashboard data function

--- orbit-api/functions/get-dashboard-data.js
+++ orbit-api/functions/get-dashboard-data.js
@@ -1,5 +1,16 @@
 const dashboardData = require("../data/dashboard");
+const { checkAuth } = require("../util");
 
 exports.handler = async function (event, context) {
-  return { statusCode: 200, body: JSON.stringify(dashboardData) };
+  try {
+    await checkAuth(event);
+    return { statusCode: 200, body: JSON.stringify(dashboardData) };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Not authenticated"
+      })
+    };
+  }
 };
