45001c163139da193255a5d24896c58067b2a2b8
German Solano <kontrolg1239@gmail.com>
2022-05-27 09:11:25 -0400

    feat: :sparkles: Add endpoint to obtain orbit id for Auth0 user

--- pages/api/[[...slug]].ts
+++ pages/api/[[...slug]].ts
@@ -2,6 +2,7 @@ import requireScope from "api/middleware/requireScope";
 import { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 import * as dashboardData from "./data/dashboard";
+import prisma from "api/prisma";
 
 const app = nextConnect();
 const router = nextConnect();
@@ -10,9 +11,37 @@ router.get(
   "/dashboard-data",
   nextConnect()
     .use(requireScope(["read:dashboard"]))
-    .use<NextApiRequest, NextApiResponse>((req, res) =>
-      res.json(dashboardData)
-    ) as any
+    .use<NextApiRequest, NextApiResponse>(async (req, res) => {
+      return res.json(dashboardData);
+    }) as any
+);
+
+// TODO: Pedir el token de Auth0 y validarlo.
+router.post<NextApiRequest, NextApiResponse>(
+  "/orbit-id-for-user",
+  async (req, res) => {
+    try {
+      const { email, firstName, lastName, userId } = req.body;
+      const user = await prisma.user.findUnique({ where: { userId } });
+
+      if (user) {
+        return res.json({ orbit_id: user.id });
+      }
+
+      const newUser = await prisma.user.create({
+        data: {
+          email,
+          firstName,
+          lastName,
+          userId
+        }
+      });
+
+      res.json({ orbit_id: newUser.id });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 );
 
 app.use("/api", router);
