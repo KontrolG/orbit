61ea865cc557ed0509f3deb1cc04a1d34ca374ec
German Solano <kontrolg1239@gmail.com>
2022-06-04 07:35:28 -0400

    fix: :bug: Fix inventory endpoints

--- pages/api/inventory/index.js
+++ pages/api/inventory/index.js
@@ -1,53 +1,52 @@
-import nextConnect from 'next-connect';
-import database from '../../../backend/middleware/database';
-import { ObjectId } from 'mongodb';
+import nextConnect from "next-connect";
+import prisma from "backend/prisma";
+import requireScope from "backend/middleware/requireScope";
 
 const handler = nextConnect();
 
-handler.use(database);
+handler.get(
+  nextConnect()
+    .use(requireScope(["read:inventory"]))
+    .use(async (req, res) => {
+      try {
+        const { sub } = req.session.user;
+        const data = await prisma.inventoryItem.findMany({
+          where: { user: sub }
+        });
+        res.json(data);
+      } catch (err) {
+        console.log("the err", err);
+        res.status(400).json({ message: "Something went wrong" });
+      }
+    })
+);
 
-handler.get(async (req, res) => {
-  try {
-    const { sub } = req.user;
-    const data = await req.db
-      .collection('inventory-items')
-      .find({ user: ObjectId(sub) })
-      .toArray();
-    res.json(data);
-  } catch (err) {
-    console.log('the err', err);
-    res
-      .status(400)
-      .json({ message: 'Something went wrong' });
-  }
-});
+handler.post(
+  nextConnect()
+    .use(requireScope(["write:inventory"]))
+    .use(async (req, res) => {
+      try {
+        const { sub } = req.session.user;
+        const data = Object.assign({}, req.body, {
+          user: sub,
+          image:
+            "https://images.unsplash.com/photo-1580169980114-ccd0babfa840?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9"
+        });
 
-handler.post(async (req, res) => {
-  try {
-    const { sub } = req.user;
-    const input = Object.assign({}, req.body, {
-      user: ObjectId(sub),
-      image:
-        'https://images.unsplash.com/photo-1580169980114-ccd0babfa840?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=800&h=600&fit=crop&ixid=eyJhcHBfaWQiOjF9'
-    });
+        const insertedInventoryItem = await prisma.inventoryItem.create({
+          data
+        });
 
-    const inventoryItem = await req.db
-      .collection('inventory-items')
-      .insertOne(input);
-
-    const insertedInventoryItem = await req.db
-      .collection('inventory-items')
-      .findOne({ _id: ObjectId(inventoryItem.insertedId) });
-
-    res.status(201).json({
-      message: 'Inventory item created!',
-      inventoryItem: insertedInventoryItem
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: 'There was a problem creating the item'
-    });
-  }
-});
+        res.status(201).json({
+          message: "Inventory item created!",
+          inventoryItem: insertedInventoryItem
+        });
+      } catch (err) {
+        return res.status(400).json({
+          message: "There was a problem creating the item"
+        });
+      }
+    })
+);
 
 export default handler;
